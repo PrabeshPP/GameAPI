195860d5370841d5f64fab61ad0736872d298fc4
Prabesh Bista <83439329+PrabeshPP@users.noreply.github.com>
2024-03-21 16:45:39 -0400

    [adding] Data Transfer Objects

---
+++ GameAPI/Dtos/GameDTos.cs
@@ -0,0 +1,3 @@
+﻿namespace GameAPI;
+
+public record class GameDTos(int Id, string Name, string Genre, decimal Price, DateOnly ReleaseDate);
--- GameAPI/Program.cs
+++ GameAPI/Program.cs
@@ -1,6 +1,15 @@
+using GameAPI;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+List<GameDTos> games = new List<GameDTos>{
+    new (1,"FIFA","Sports",79.99M, new DateOnly(2022,9,27)),
+    new (2,"COD","Action",58.99M, new DateOnly(2022,9,27)),
+    new (3,"GTA V","Action",100.99M, new DateOnly(2022,9,27)),
+};
+
+app.MapGet("/games",()=>games);
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
